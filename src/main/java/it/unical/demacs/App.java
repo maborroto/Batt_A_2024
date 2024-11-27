0b9c74bcac9ceec649a1d46d8b6a9fdb7f6c42cd
maborroto <maborroto1987@gmail.com>
2024-11-27 11:52:26 +0100

    Revert "Adding algorithms"
    This reverts commit 957ec1173029b9f05d9058fb7859687132254789.

--- src/main/java/it/unical/demacs/App.java
+++ src/main/java/it/unical/demacs/App.java
@@ -1,14 +1,10 @@
 package it.unical.demacs;
 
+/**
+ * Hello world!
+ */
 public class App {
     public static void main(String[] args) {
-        Algorithms alg = new Algorithms();
-        String sent = "Angolo bar a Bologna";
-        System.out.println("Is Palindrome: " + alg.isPalindrome(sent));
-
-        int[] numbers = new int[]{1, 5, 8, 9, 50, 60, 80, 90, 101};
-        int target = 50;
-        int found = alg.binarySearch(numbers, target);
-        System.out.println(found != -1 ? target + " found at: " + found : target + " not found");
+        System.out.println("Hello World!");
     }
 }
