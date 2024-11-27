7bb4eb599218dd8d905765e5d7e4fb17f805d2da
maborroto <maborroto1987@gmail.com>
2024-11-27 12:03:14 +0100

    redo

---
+++ src/main/java/it/unical/demacs/Algorithms.java
@@ -0,0 +1,58 @@
+package it.unical.demacs;
+
+public class Algorithms {
+
+    /**
+     * Checks if a given sentence is palindrome or not
+     *
+     * @param str It is a String that cannot be null
+     * @return a boolean value
+     * @throws IllegalArgumentException if the input parameter is null
+     */
+    public boolean isPalindrome(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException("Input string cannot be null");
+        }
+        if (str.length() < 2) {
+            return true;
+        }
+        str = str.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        while (left < (str.length() - 1) / 2) {
+            if (str.charAt(left) != str.charAt(str.length() - 1 - left)) {
+                return false;
+            }
+            left++;
+        }
+        return true;
+    }
+
+    /**
+     * +
+     * Given a non-null ordered array of integers, searches for the target number in the array
+     *
+     * @param numbers Non-null array of numbers
+     * @param target  Number to be found in the array
+     * @return If the target is in the array, it returns its index, otherwise -1
+     * @throws IllegalArgumentException if the numbers array is null
+     */
+    public int binarySearch(int[] numbers, int target) {
+        if (numbers.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (numbers[mid] == target) {
+                return mid;
+            }
+            if (numbers[mid] > target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
--- src/main/java/it/unical/demacs/App.java
+++ src/main/java/it/unical/demacs/App.java
@@ -5,6 +5,12 @@ package it.unical.demacs;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Algorithms alg = new Algorithms();
+        String sent = "Angolo bar a Bologna";
+        System.out.println("Is Palindrome: " + alg.isPalindrome(sent));
+        int[] numbers = new int[]{1, 5, 8, 9, 50, 60, 80, 90, 101};
+        int target = 50;
+        int found = alg.binarySearch(numbers, target);
+        System.out.println(found != -1 ? target + " found at: " + found : target + " not found");
     }
 }
